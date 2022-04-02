cc86e646dbfaf1abb99cbe17ca13618a9869e896
Bruno Staine <brunostaine@hotmail.com>
2022-04-02 15:36:42 -0300

    db teste

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/db', (req, res) => {
+    res.json({
+        p1: 'Qual é o nome do Aluno',
+        p2: 'Idade do Aluno'
+    })
+})
+
+app.get('/login', (req, res) => {
+    res.json({
+        nome: 'Bruno',
+        sobrenome: 'Staine',
+        email: 'brunostaine@hotmail.com',
+        senha: '123'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('Escutando na porta: ' + PORT)
+})
